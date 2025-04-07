598ab0ba798fba07252727829965fbd561505ad1
MEGA <MEGA@gmail.com>
2025-04-07 13:28:16 +0300

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете числа, разделени с интервали:");
+            string input = Console.ReadLine();
+
+            string[] inputStrings = input.Split(' ');
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+
+            foreach (string num in inputStrings)
+            {
+                uniqueNumbers.Add(int.Parse(num));
+            }
+
+            List<int> sortedUniqueNumbers = new List<int>(uniqueNumbers);
+            sortedUniqueNumbers.Sort();
+
+            Console.WriteLine("Уникалните числа са:");
+            Console.WriteLine(string.Join(" ", sortedUniqueNumbers));
+        }
+    }
+}
